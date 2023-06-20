40126caa38d398dab7c3cf78d8210e075be8b13d
danielk <daniel.kastinen@irf.se>
2023-06-20 11:12:49 +0200

    source and test files

---
+++ tests/test_package.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+'''
+Test package
+'''
+import unittest
+import numpy as np
+import numpy.testing as nt
+
+
+class TestImport(unittest.TestCase):
+
+    def test_variables(self):
+        import resordan
