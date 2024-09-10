ef12213f9bc1e3344b07ee469faf911fb669deb2
Ingar Arntzen <inar@norceresearch.no>
2024-09-10 16:02:11 +0200

    made a simplified download_tle

--- src/resordan/correlator/main.py
+++ src/resordan/correlator/main.py
@@ -1,5 +1,35 @@
 import argparse
 from pathlib import Path
+import spacetrack
+import datetime as dt
+
+ISO_FMT = '%Y-%m-%dT%H:%M:%S'
+
+def download_tle(start_dt, end_dt, st_credentials, dst=None):
+    """
+    download TLE for time interval, store to folder dst if given
+    """
+    drange = spacetrack.operators.inclusive_range(start_dt, end_dt)
+    user = st_credentials["user"]
+    passwd = st_credentials["passwd"]
+    st = spacetrack.SpaceTrackClient(identity=user, password=passwd)
+    lines = st.tle_publish(
+        iter_lines=True, 
+        orderby='TLE_LINE1', 
+        format='tle',
+        publish_epoch=drange
+    )
+    if not dst:
+        return lines
+    else:
+        start = dt.datetime.strftime(start_dt, ISO_FMT)
+        end = dt.datetime.strftime(end_dt, ISO_FMT)
+        outfile = Path(dst) / f"{start}.{end}.tle"
+        # write to file
+        with open(outfile, 'w') as file:
+            for line in lines:
+                file.write(line + "\n")
+
 
 def main(input_args=None):
     parser = argparse.ArgumentParser(description='Correlate Detections')
@@ -14,5 +44,16 @@ def main(input_args=None):
     print("WORK IN PROGRESS")
 
 
+
+
+
+
 if __name__ == '__main__':
-    main()
+
+
+    def test_discos():
+        # DISCOS
+        TOKEN = "ImE2NGE5Y2YwLWUxMTEtNDg2NS04ZTYxLTRhZmQ0OTBiN2VjNyI.pN0jXjkaLyz0GsRFmYliLcQTtl4"
+
+
+
