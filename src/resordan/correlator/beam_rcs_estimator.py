6984ca627381e8b052426f609f6ff5c7ff5a23b2
Ingar Arntzen <ingar.arntzen@gmail.com>
2024-10-21 10:10:56 +0200

    cleaned formatting in beam_rcs_estimator

--- src/resordan/correlator/beam_rcs_estimator.py
+++ src/resordan/correlator/beam_rcs_estimator.py
@@ -16,7 +16,6 @@ import h5py
 
 import sorts
 import pyant
-import pyorb
 import resordan.correlator.update_tle as utle
 from resordan.correlator.discos_cat import get_discos_objects
 
@@ -27,12 +26,11 @@ except ImportError:
     class COMM_WORLD:
         rank = 0
         size = 1
-        
+
         def Barrier(self):
             pass
 
     comm = COMM_WORLD()
-
 
 
 ###################################################################
@@ -72,12 +70,12 @@ def matching_function(data, SNR_sim, off_angles, min_snr, verbose=False):
     max_sndb_x = np.nanmax(data.snr)
     max_sndb_y = np.log10(np.nanmax(_SNR_sim))*10
 
-    #ratio of peak signal and limit should be the same
-    #as the ratio of the simulated peak and the simulated signal
-    #use this to calculate the limit used in simulation
+    # ratio of peak signal and limit should be the same
+    # as the ratio of the simulated peak and the simulated signal
+    # use this to calculate the limit used in simulation
     snrdb_lim_rel = max_sndb_x - min_snr
     sns_lim = max_sndb_y - snrdb_lim_rel
-    #the normalized limit is, since the normalizations put peaks to 1
+    # the normalized limit is, since the normalizations put peaks to 1
     snrdb_lim_norm = -snrdb_lim_rel
 
     off_weight = offaxis_weighting(off_angles)
@@ -98,7 +96,7 @@ def matching_function(data, SNR_sim, off_angles, min_snr, verbose=False):
 
     idx = np.logical_and(idx_x, idx_y)
     tot = np.sum(idx)
-    
+
     dhit_idx = np.logical_and.reduce([
         sndb > min_snr,
         snsdb > sns_lim,
@@ -114,11 +112,11 @@ def matching_function(data, SNR_sim, off_angles, min_snr, verbose=False):
         np.logical_or(snsdb <= sns_lim, np.logical_not(idx_y)),
         idx_x,
     ])
-    
+
     dhit = np.nan
     dcut = np.nan
     dmiss = np.nan
-        
+
     if tot > 1:
         if np.any(dhit_idx):
             dhit = np.sum(((xsn[dhit_idx] - ysn[dhit_idx])*off_weight[dhit_idx])**2)
@@ -126,7 +124,7 @@ def matching_function(data, SNR_sim, off_angles, min_snr, verbose=False):
             dcut = np.sum(((ysn[dcut_idx] - snrdb_lim_norm)*off_weight[dcut_idx])**2)
         if np.any(dmiss_idx):
             dmiss = np.sum((xsn[dmiss_idx]*off_weight[dmiss_idx])**2)
-    
+
     match = dhit
     if not np.isnan(dcut):
         match += dcut
@@ -137,15 +135,15 @@ def matching_function(data, SNR_sim, off_angles, min_snr, verbose=False):
     meta = [dcut/len(SNR_sim), dmiss/len(SNR_sim)]
 
     timearray = data.t
-    xxx = np.array([timearray[idx],xsn[idx]]).T
-    yyy = np.array([timearray[idx],ysn[idx]]).T
+    xxx = np.array([timearray[idx], xsn[idx]]).T
+    yyy = np.array([timearray[idx], ysn[idx]]).T
     rowy, coly = np.shape(yyy)
 
     if rowy < 4:
         mae = float('NaN')
     else:
-        mae = similaritymeasures.mae(xxx[:],yyy[:])
-    
+        mae = similaritymeasures.mae(xxx[:], yyy[:])
+
     if verbose:
         print('idx_x: ', idx_x)
         print('idx_y: ', idx_y)
@@ -165,22 +163,21 @@ def matching_function(data, SNR_sim, off_angles, min_snr, verbose=False):
         match = float('NaN')
     else:
         match = match
-        
+
     return match, meta, mae
 
 
 def snr2rcs(
-            gain_tx,
-            gain_rx,
-            wavelength,
-            power_tx,
-            range_tx_m,
-            range_rx_m,
-            snr,
-            bandwidth=10,
-            rx_noise_temp=150.0,
-            radar_albedo=1.0,
-        ):
+        gain_tx,
+        gain_rx,
+        wavelength,
+        power_tx,
+        range_tx_m,
+        range_rx_m,
+        snr,
+        bandwidth=10,
+        rx_noise_temp=150.0,
+        radar_albedo=1.0):
 
     """
     Use the radar equation to compute RCS values from SNR data
@@ -189,9 +186,9 @@ def snr2rcs(
     rx_noise = scipy.constants.k*rx_noise_temp*bandwidth
     power = snr*rx_noise/radar_albedo
 
-    rcsvalue = 64.0*(np.pi**3.0)*(range_rx_m**2.0*range_tx_m**2.0)*power/(power_tx*(gain_tx*gain_rx)*(wavelength**2.0))
-
-    return rcsvalue
+    rcs_dividend = 64.0 * (np.pi**3.0) * (range_rx_m**2.0*range_tx_m**2.0) * power
+    rcs_divisor = power_tx*(gain_tx*gain_rx)*(wavelength**2.0)
+    return rcs_dividend / rcs_divisor
 
 
 def generate_prediction(data, t, obj, radar, min_gain):
@@ -200,7 +197,7 @@ def generate_prediction(data, t, obj, radar, min_gain):
     Compute the RCS values
     """
 
-    states_ecef = obj.get_state(t) # /sorts/space_object.py & /sorts/propagator/pysgp4.py
+    states_ecef = obj.get_state(t)  # /sorts/space_object.py & /sorts/propagator/pysgp4.py
     ecef_r = states_ecef[:3, :] - radar.tx[0].ecef[:, None]
 
     local_pos = sorts.frames.ecef_to_enu(
@@ -212,33 +209,33 @@ def generate_prediction(data, t, obj, radar, min_gain):
     )
 
     pth = local_pos/np.linalg.norm(local_pos, axis=0)
-    G_pth = radar.tx[0].beam.gain(pth) # $ENV/site-packages/pyant/beams/eiscat_uhf.py
+    G_pth = radar.tx[0].beam.gain(pth)  # $ENV/site-packages/pyant/beams/eiscat_uhf.py
     G_pth = np.squeeze(G_pth)
     G_pth_db = 10*np.log10(G_pth)
 
     antilogsnr = 10**(data.snr/10)
-    
+
     rcsvalue = snr2rcs(
         G_pth,
         G_pth,
         radar.tx[0].wavelength,
         radar.tx[0].power,
-        data.range, # in meters, 1way
-        data.range, # in meters, 1way
-        antilogsnr, # Not in dB
+        data.range,  # in meters, 1way
+        data.range,  # in meters, 1way
+        antilogsnr,  # Not in dB
         bandwidth=radar.tx[0].coh_int_bandwidth,
         rx_noise_temp=radar.rx[0].noise,
         radar_albedo=1.0,
     )
-    
+
     diam = sorts.signals.hard_target_diameter(
         G_pth,
         G_pth,
         radar.tx[0].wavelength,
         radar.tx[0].power,
-        data.range, # in meters, 1way
-        data.range, # in meters, 1way
-        antilogsnr, # Not in dB
+        data.range,  # in meters, 1way
+        data.range,  # in meters, 1way
+        antilogsnr,  # Not in dB
         bandwidth=radar.tx[0].coh_int_bandwidth,
         rx_noise_temp=radar.rx[0].noise,
         radar_albedo=1.0,
@@ -249,23 +246,23 @@ def generate_prediction(data, t, obj, radar, min_gain):
         G_pth,
         radar.tx[0].wavelength,
         radar.tx[0].power,
-        data.range, # in meters, 1way
-        data.range, # in meters, 1way
+        data.range,  # in meters, 1way
+        data.range,  # in meters, 1way
         diameter=1,
         bandwidth=radar.tx[0].coh_int_bandwidth,
         rx_noise_temp=radar.rx[0].noise,
         radar_albedo=1.0,
     )
-    
+
     low_gain_inds = G_pth_db < min_gain
     diam[low_gain_inds] = np.nan
     SNR_sim[low_gain_inds] = 0
     rcsvalue[low_gain_inds] = np.nan
 
     return SNR_sim, G_pth, diam, low_gain_inds, ecef_r, pth, rcsvalue
-    
-    
-def updated_tle(line1,line2,data,radar):
+
+
+def updated_tle(line1, line2, data, radar):
     """
     Update TLE information of objects in catalogue
     """
@@ -273,15 +270,16 @@ def updated_tle(line1,line2,data,radar):
     line1 = (line1.astype(str))[0]
     line2 = (line2.astype(str))[0]
 
-    r  = (data.range)*2 # in m, 2way approach used in Daniel's program
-    v  = (data.range_rate)*2 # in m/s, 2way approach used in Daniel's program
+    r = (data.range) * 2  # in m, 2way approach used in Daniel's program
+    v = (data.range_rate) * 2  # in m/s, 2way approach used in Daniel's program
 
-    times  = Time(data.epoch + data.t , format="unix", scale="utc") # in unix
+    times = Time(data.epoch + data.t, format="unix", scale="utc")  # in unix
     rx_ecef, tx_ecef = utle.load_radar_ecef(radar)
 
     new_tle = utle.update_tle(line1, line2, times, r, v, rx_ecef, tx_ecef)
 
     return new_tle
+
 
 def plot_estimator_results(
         data, 
@@ -298,8 +296,7 @@ def plot_estimator_results(
         diam, 
         pth, 
         fileformat, 
-        results_folder
-    ):
+        results_folder):
     """
     Plot parameters that resulted from RCS_ESTIMATOR
     """
@@ -311,10 +308,10 @@ def plot_estimator_results(
     pyant.plotting.gain_heatmap(radar.tx[0].beam, min_elevation=85.0, ax=ax)
     fig.savefig(results_folder / f'correlated_pass_pth_gain.{fileformat}')
     plt.close(fig)
-        
+
     fig, axes = plt.subplots(2, 2, figsize=(16, 8))
     axes[0, 0].plot(t_jitter, matches, '.k')
-    axes[0, 0].plot(t_jitter, pmae,'k')
+    axes[0, 0].plot(t_jitter, pmae, 'k')
     if bmae == bmae:
         axes[0, 0].plot(t_jitter[bmae], pmae[bmae], 'or')
     axes[0, 0].set_xlabel('Jitter [s]')
@@ -343,6 +340,7 @@ def plot_estimator_results(
     fig.savefig(results_folder / f'correlated_pass_snr_match.{fileformat}')
     plt.close(fig)
 
+
 def save_estimator_results(
         discos_map, 
         norad, 
@@ -352,8 +350,7 @@ def save_estimator_results(
         pth, 
         rcs_data, 
         data, 
-        results_folder
-    ):
+        results_folder):
     """
     Save measured and simulated time series for each object with 
     their respective geometries and shapres retrieved from DISCOS
@@ -362,17 +359,20 @@ def save_estimator_results(
     # discos object
     _catid = norad
     discos_tup = discos_map[_catid]
+    (
+        nameo, satno, oClass, mission, mass, 
+        shape, width, height, depth, diam, 
+        span, xsMax, xsMin, xsAvg
+    ) = discos_tup
 
-    nameo, satno, oClass, mission, mass, shape, width, height, depth, diam, span, xsMax, xsMin, xsAvg = discos_tup
-        
     summary_data = dict(
                 SNR_sim = SNR_sim,
                 diam_sim = diam_sim,
                 gain_sim = G_pth,
                 pth_sim = pth,
-                #measurement_id = meas_id,
-                #object_id = obj_id,
-                #offset_angle = offset_angle,
+                # measurement_id = meas_id,
+                # object_id = obj_id,
+                # offset_angle = offset_angle,
                 catid = satno,
                 rcs_data = rcs_data,
                 snr_data = data.snr,
@@ -391,9 +391,10 @@ def save_estimator_results(
                 xSectMin = xsMin,
                 xSectAvg = xsAvg,
     )
-        
-    with open(results_folder / f'correlated_snr_prediction.pickle', 'wb') as fh:
+
+    with open(results_folder / 'correlated_snr_prediction.pickle', 'wb') as fh:
         pickle.dump(summary_data, fh)
+
 
 ###################################################################
 # RCS ESTIMATE
@@ -410,11 +411,10 @@ def rcs_estimator(
         min_gain=10.0,
         min_snr=5.0,
         fileformat='png',
-        verbose=False
-    ):
+        verbose=False):
     """
     Compute Radar Cross Section and estinate SNR and diameter from measured SNR 
- 
+
     Params
     ------
 
@@ -434,7 +434,7 @@ def rcs_estimator(
     """
 
     radar = getattr(sorts.radars, radarid)
-    
+
     t_jitter = np.arange(
         -jitter_width, 
         jitter_width, 
@@ -447,9 +447,9 @@ def rcs_estimator(
     path2correvents = Path(correlation_events).resolve()
     path2corrdata = Path(correlation_data).resolve()
     tle_pth = Path(catalog).resolve()
-        
+
     corr_events = list(sorted(path2correvents.rglob('*.pkl')))
-    
+
     pop = sorts.population.tle_catalog(tle_pth, cartesian=False)
     pop.unique()
 
@@ -458,17 +458,17 @@ def rcs_estimator(
         if verbose:
             print(path2corrdata / corrdata_filename)
             print(corr_event)
-        
+
         with h5py.File(path2corrdata / corrdata_filename, 'r') as ds:
             indecies = ds['matched_object_index'][()][0, :]
             measurnment_id = ds['observation_index'][()]
             time_id = ds['matched_object_time'][()]
             selected = ds['correlated'][()]
-            indecies = indecies[measurnment_id] # sort according to detection
+            indecies = indecies[measurnment_id]  # sort according to detection
             time_id = time_id[measurnment_id]
             selected = selected[measurnment_id]
             measurnment_id = np.arange(len(measurnment_id))
-            
+
         with open(str(corr_event), 'rb') as fip:
             h_det = pickle.load(fip)
 
@@ -480,14 +480,14 @@ def rcs_estimator(
             snrs = events.snr
             snridmax = np.nanargmax(snrs)
             ts = eepoch + events.t
-            t = ts[snridmax] # time at the max SNR  
+            t = ts[snridmax]  # time at the max SNR  
             select_id = np.where(time_id == t)
             select_id = select_id[0]
             obj_id = indecies[select_id][0]
             norads.append(pop.data['oid'][obj_id])
 
         norads = np.array(norads)
-        catids = norads[selected] # succesful correlations. Distance < thrreshold
+        catids = norads[selected]  # succesful correlations. Distance < thrreshold
         discos_map = get_discos_objects(catids, token)
 
         pbar = tqdm(
@@ -504,14 +504,14 @@ def rcs_estimator(
                 snridmax = np.nanargmax(snrs)
                 ts = eepoch + events.t
                 rs = events.range
-                t = ts[snridmax] # time at the max SNR
-                r = rs[snridmax] # in m, one way
-                az = events.pointing[0][0] # assuming that all values are the same
+                t = ts[snridmax]  # time at the max SNR
+                r = rs[snridmax]  # in m, one way
+                az = events.pointing[0][0]  # assuming that all values are the same
                 ele = events.pointing[0][1]
-                
+
                 select_id = np.where(time_id == t)
                 select_id = select_id[0]
-        
+
                 meas_id = measurnment_id[select_id][0]
                 obj_id = indecies[select_id][0]
                 toutc = datetime.fromtimestamp(eepoch, UTC).strftime('%Y%m%d_%H%M%S_%f')
@@ -530,62 +530,65 @@ def rcs_estimator(
                 if norad < 81000:
                     results_folder = output_pth / ename
                     results_folder.mkdir(exist_ok=True)
-            
+
                     radar.tx[0].beam.sph_point(azimuth = az, elevation = ele)
-                    
-                    ##==================
-                    ## TLE update # The update does not produce simulations
-                    ##==================
-                    #line1 = pop.data[np.where(pop.data['oid'] == norad)]['line1']
-                    #line2 = pop.data[np.where(pop.data['oid'] == norad)]['line2']
-                    #print(line1)
-                    #print(line2)
-                    #new_tle = updated_tle(line1,line2,data,radarid) # corr_event or data
-                    #print(new_tle)
-                    #new_pop = sorts.population.tle_catalog([(new_tle[0],new_tle[1])], cartesian=False)
-                    #obj = new_pop.get_object(0)
-                    ##==================
-                    
+
+                    # ==================
+                    # TLE update # The update does not produce simulations
+                    # ==================
+                    # line1 = pop.data[np.where(pop.data['oid'] == norad)]['line1']
+                    # line2 = pop.data[np.where(pop.data['oid'] == norad)]['line2']
+                    # print(line1)
+                    # print(line2)
+                    # new_tle = updated_tle(line1,line2,data,radarid) # corr_event or data
+                    # print(new_tle)
+                    # new_pop = sorts.population.tle_catalog(
+                    #     [(new_tle[0],new_tle[1])], cartesian=False
+                    # )
+                    # obj = new_pop.get_object(0)
+                    # ==================
+
                     obj.out_frame = 'ITRS'
 
                     t = Time(t, format='unix', scale='utc')
-                    dt = (t - obj.epoch).sec # unix time - mjd. format. Use astropy
+                    dt = (t - obj.epoch).sec  # unix time - mjd. format. Use astropy
                     t_vec = data.t - data.t[snridmax]
 
                     matches = np.empty_like(t_jitter)
                     pdatas = [None]*len(t_jitter)
                     pmetas = [None]*len(t_jitter)
                     pmae = [None]*len(t_jitter)
-                    
+
                     for tind in range(len(t_jitter)):
                         _t = t_vec + dt + t_jitter[tind]
                         pdatas[tind] = generate_prediction(data, _t, obj, radar, min_gain)
                         SNR_sim, G_pth, diam, LG_ind, ecef_r, pth, rcs_data = pdatas[tind]
-                            
+
                         nrows, ncols = np.shape(pth)
                         pths_off_angle = pyant.coordinates.vector_angle(
                             np.repeat(radar.tx[0].beam.pointing, ncols, axis=1),
                             pth,
                             degrees=True
                         )
-                        
+
                         matches[tind], pmetas[tind], pmae[tind] = matching_function( 
                                 data, SNR_sim, pths_off_angle, min_snr)
 
-                    #best_match = np.nanargmin(matches)
+                    # best_match = np.nanargmin(matches)
                     best_mae = np.nanargmin(pmae)
                     if verbose:
                         print(best_mae)
 
                     SNR_sim, G_pth, diam, LG_ind, ecef_r, pth, rcs_data = pdatas[best_mae]
-                    
+
                     # Currently, the lines commented below are not used
-                    #offset_angle = pyant.coordinates.vector_angle(pth[:, snridmax],
+                    # offset_angle = pyant.coordinates.vector_angle(pth[:, snridmax],
                     #                radar.tx[0].beam.pointing,
                     #                degrees=True
-                    #)
+                    # )
 
-                    plot_estimator_results(data, norad, radar, t_jitter, matches,
+                    plot_estimator_results(
+                        data, norad, radar, t_jitter, matches,
                         best_mae, pmae, SNR_sim, snridmax, ecef_r, r, diam, pth,
                         fileformat, results_folder
                     )
@@ -608,7 +611,7 @@ def rcs_estimator(
 def main(input_args=None):
     parser = argparse.ArgumentParser(description='Estimate RCS \
         values for target using correlation and TLE')
-        
+
     parser.add_argument('radar', type=str, help='The observing radar system')
     parser.add_argument('catalog', type=str, help='TLE catalog path')
     parser.add_argument('correlation_events', type=str, help='Path to \
@@ -640,7 +643,7 @@ def main(input_args=None):
     )
     parser.add_argument(
         '--jitter-width',
-        default=1.5, # it was 5.0
+        default=1.5,  # it was 5.0
         metavar='JITTER',
         type=float, 
         help='The number of seconds to jitter back and forth',
@@ -655,11 +658,11 @@ def main(input_args=None):
         default='png',
         help='Plot format',
     )
-    
+
     credentials = 'discos_credentials.txt'
     proc = subprocess.Popen("sed -n '1p' "+credentials, stdout=subprocess.PIPE, shell=True)
     token = proc.stdout.read()
-    token = token.strip().decode( "utf-8" )
+    token = token.strip().decode("utf-8")
 
     if input_args is None:
         args = parser.parse_args()
