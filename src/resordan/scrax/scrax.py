ff09e36b481d8c0d747dace14069751031ece618
Ingar Arntzen <inar@norceresearch.no>
2024-11-07 15:26:47 +0100

    implementing scrax and unittest

--- src/resordan/scrax/scrax.py
+++ src/resordan/scrax/scrax.py
@@ -1,7 +1,76 @@
+from pathlib import Path
+import pickle
+
+SNR_PREDICTION_FILENAME = "correlated_snr_prediction.pickle"
 
 
-def scrax(input, logger=None):
+def size_shape_estimator(rcs_data, model):
+    """
+    size shape estimation for the rcs data of a single pass
+    """
+    return {"estimate": 3.14}
 
+
+def tasks_size_shape_estimator(src):
+    """generate estimation tasks for a src product"""
+    tasks = []
+    for satdir in [d for d in Path(src).iterdir() if d.is_dir()]:
+        for passdir in [d for d in satdir.iterdir() if d.is_dir()]:
+            snr_prediction_file = passdir / SNR_PREDICTION_FILENAME
+            if snr_prediction_file.is_file():
+                tasks.append((satdir.name, passdir.name, str(snr_prediction_file)))
+    return tasks
+
+def process_size_shape_estimator(tasks, model):
+    """
+    process size shape estimation for a list of tasks
+    (satid, passid, snr_prediction_file)
+    """    
+    results = []
+    for satid, passid, snr_prediction_file in tasks:
+        with open(snr_prediction_file, 'rb') as file:
+            data = pickle.load(file)
+            estimate = size_shape_estimator(data["rcs_data"], model)
+            results.append((satid, passid, snr_prediction_file, estimate))
+    return results
+
+
+def scrax(data_dir, model_file, logger=None):
+    """
+    main entrypoint scrax
+    """
     if logger:
         logger.info("Hellow World, Scrax")
+
+    # generate estimation tasks
+    tasks = tasks_size_shape_estimator(data_dir)
+
+    # load model
+    with open(model_file, 'rb') as f:
+        model = pickle.load(f)
+
+    # process
+    results = process_size_shape_estimator(tasks, model)
+
+    # store results
+    for satid, passid, snr_prediction_file, estimate in results:
+        with open(snr_prediction_file, 'rb') as file:
+            data = pickle.load(file)
+            print(data["estimate"])
+            data["estimate"] = estimate
+        with open(snr_prediction_file, 'wb') as file:
+            pickle.dump(data, file)
+
+
+
+
+if __name__ == '__main__':
+
+    # test input
+    src = "/cluster/projects/p106119-SpaceDebrisRadarCharacterization/rcs/leo_bpark_2.1u_EI-20240822-UHF"
+    src = "./tmp"
+    model = "correlated_snr_prediction.pickle"
+    scrax(src, model)
+
+
     
--- tests/test_scrax_rcs.py
+++ tests/test_scrax_rcs.py
@@ -3,22 +3,17 @@ import numpy as np
 import matplotlib
 from matplotlib import pyplot as plt
 from resordan.scrax.get_circular_plate_rcs import getCircularPlateRCS
-
-# Fixture to set up matplotlib for all tests
-@pytest.fixture(autouse=True)
-def setup_matplotlib():
-    # Set the matplotlib backend to 'Agg' (or any other backend)
-    # 'Agg' is typically used for non-interactive testing (without GUI)
-    matplotlib.use('TkAgg')
-
-    # You could also reset figures before each test, if needed
-    yield
-    # Cleanup code (if necessary), e.g., closing all figures
-    plt.close('all')
+from resordan.scrax.scrax import process_size_shape_estimator
+import pickle
+import tempfile
+from pathlib import Path
 
 
 @pytest.mark.interactive
 def test_circular_plate(setup_matplotlib):
+
+    matplotlib.use('TkAgg')
+
 
     # vhf = 2.24e8  # EISCAT VHF radar frequency [Hz]
     uhf = 9.30e8  # EISCAT UHF radar frequency [Hz]
@@ -38,4 +33,50 @@ def test_circular_plate(setup_matplotlib):
     plt.plot(el.flatten(), rcs_dB.flatten())
     plt.show()
 
+    plt.close('all')
+
     assert True
+
+
+def test_snr_prediction():
+
+    rcs_data = np.array([
+        1.26037036e+00, 8.49878611e+00, 4.74602680e+01, 1.63479929e+01,
+        3.27654639e+00, 1.93519575e+00, 7.28008166e-01, 3.10218118e+00,
+        6.60246862e+00, 1.77719047e+00, 1.70862592e+00, 8.05324053e-01,
+        6.60713679e-01, 7.82819190e-01, 7.69305285e-01, 2.76347415e-01,
+        9.87335502e+01, 1.75266724e+01, 5.76228048e+00, 3.12110456e+00,
+        2.55969778e+00, 5.71462026e-02, 6.16913940e-02, 2.71260586e-02,
+        np.nan, 3.17948739e-02, 8.58343048e-01, 1.41288590e-01
+    ])
+
+    # make mockup filt
+    data_dict = {"rcs_data": rcs_data}    
+    
+    with tempfile.TemporaryDirectory() as temp_dir:
+
+        model_file = Path(temp_dir) / "model.pickle"
+        snr_file = Path(temp_dir) / "snr.pickle"
+
+        # make mockup file for rcs data
+        with open(snr_file, "wb") as f:
+            pickle.dump(data_dict, f)
+
+        # make mockup file for model
+        with open(model_file, "wb") as f:
+            pickle.dump(data_dict, f)
+
+        # make task list
+        tasks = [("satid", "passid", str(snr_file))]
+
+        # load model
+        with open(snr_file, "rb") as f:
+            model = pickle.load(f)
+
+        # process
+        results = process_size_shape_estimator(tasks, model)
+        assert len(results) > 0
+        satid, passid, fname, res = results[0]
+        assert res is not None
+
+
